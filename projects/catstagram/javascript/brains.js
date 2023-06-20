8699489a94f9c820349f41741023490ef3623a1a
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-06-20 16:52:27 +0000

    todo 24 on javascript

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,13 +1,10 @@
 var counter1 = 0
 var counter2 = 0
-var counter3 = 0
-var counter4 = 0
-var counter5 = 0
-function likePhoto() {
+function likePhoto(counter1, counter2) {
     counter1 = counter1 + 1;
-    document.getElementById("likeCount1").innerHTML = counter1;;
-}
-function likePhoto2() {
     counter2 = counter2 + 1
+    document.getElementById("likeCount1").innerHTML = counter1;;
     document.getElementById("likeCount2").innerHTML = counter2;;
+
 }
+
