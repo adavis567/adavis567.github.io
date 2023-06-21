d69f8bf22e7b3df0cc69778726ee1fe1a9f6432a
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-06-21 16:47:11 +0000

    on todo 6

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 15, 15);
             view.addChild(circle);
             circles.push(circle);
         }
@@ -49,10 +49,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
 
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-
+            game.checkCirclePosition(circles[0])
+            game.checkCirclePosition(circles[1])
+            game.checkCirclePosition(circles[2])
+            game.checkCirclePosition(circles[3])
+            game.checkCirclePosition(circles[4])
 
             // TODO 9 : Iterate over the array
 
@@ -70,6 +78,7 @@ var init = function (window) {
             if (circle.x > canvas.width) {
                 circle.x = 0;
             }
+        //   If the circle has gone past the left side, then place it on the right side
 
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
 
