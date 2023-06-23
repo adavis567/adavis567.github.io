a46944a6b61fdde489db5eb37be925cc42a48d01
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-06-23 16:57:43 +0000

    on todo 24 javascript

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -1,10 +1,11 @@
 var counter1 = 0
 var counter2 = 0
 function likePhoto(counter1, counter2) {
-    counter1 = counter1 + 1;
-    counter2 = counter2 + 1
-    document.getElementById("likeCount1").innerHTML = counter1;;
-    document.getElementById("likeCount2").innerHTML = counter2;;
+    
+    if (counter1) {
+        counter1 = counter1 + 1;
+        document.getElementById("likeCount1").innerHTML = counter1;;
+    }
 
 }
 
