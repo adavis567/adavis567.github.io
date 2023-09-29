85d2d92d59126fcd815625a907aae64138e48e18
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-09-29 00:44:15 +0000

    finished walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,14 +10,31 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
-  // Game Item Objects
+  var KEY = {
+    "LEFT": 37,
+    "UP": 38,
+    "RIGHT": 39,
+    "DOWN": 40
+  }
 
+  // Game Item Objects
+  var walker = {
+    "x-coordinate": 0,
+    "y-coordinate": 0,
+    "x-speed": 0,
+    "y-speed": 0
+  }
+  var walls = {
+    "leftWall":0,
+    "rightWall":$("#board").width() - 50,
+    "topWall":0,
+    "bottomWall":$("#board").height() - 50
+  }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp)
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,22 +44,48 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem()
+    wallCollision()
+    redrawGameItem()
   }
-  
+
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.LEFT) {
+      walker["x-speed"] = -5
+    }
+    else if (event.which === KEY.UP) {
+      walker["y-speed"] = -5
+    }
+    else if (event.which === KEY.RIGHT) {
+      walker["x-speed"] = 5
+    }
+    else if (event.which === KEY.DOWN) {
+      walker["y-speed"] = 5
+    }
+  }
+  function handleKeyUp(event) {
+    if (event.which === KEY.LEFT) {
+      walker["x-speed"] = 0
+    }
+    else if (event.which === KEY.UP) {
+      walker["y-speed"] = 0
+    }
+    else if (event.which === KEY.RIGHT) {
+      walker["x-speed"] = 0
+    }
+    else if (event.which === KEY.DOWN) {
+      walker["y-speed"] = 0
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -50,5 +93,31 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+
+  function repositionGameItem() {
+    walker["x-coordinate"] += walker["x-speed"]
+    walker["y-coordinate"] += walker["y-speed"]
+  }
+
+  function redrawGameItem() {
+    $("#walker").css("left", walker["x-coordinate"])
+    $("#walker").css("top", walker["y-coordinate"])
+  }
+
+  function wallCollision() {
+    if (walls.leftWall >= walker["x-coordinate"]) {
+      walker["x-coordinate"] -= walker["x-speed"]
+      walker["y-coordinate"] -= walker["y-speed"]
+    }
+    else if (walls.rightWall <= walker["x-coordinate"]) {
+      walker["x-coordinate"] -= walker["x-speed"]
+      walker["y-coordinate"] -= walker["y-speed"]
+    }
+    else if (walls.topWall >= walker["y-coordinate"]) {
+      walker["y-coordinate"] -= walker["y-speed"]
+    }
+    else if (walls.bottomWall <= walker["y-coordinate"]) {
+      walker["y-coordinate"] -= walker["y-speed"]
+    }
+  }
 }
