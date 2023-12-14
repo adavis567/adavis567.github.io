86f1ca559acb6bdf95070a8f8c3e7b7bebcda469
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-12-14 17:26:24 +0000

    finished image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,10 +20,10 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-applyFilter(reddify)
-applyFilter(decreaseBlue)
-applyFilter(increaseGreenByBlue)
-  
+  applyFilterNoBackground(reddify)
+  applyFilterNoBackground(decreaseBlue)
+  applyFilter(increaseGreenByBlue)
+
 
   // do not change the below line of code
   render($("#display"), image);
@@ -35,7 +35,7 @@ applyFilter(increaseGreenByBlue)
 
 // TODO 1, 2 & 4: Create the applyFilter function here
 function applyFilter(filterFunction) {
-  for (var i = 0; i< image.length; i++) {
+  for (var i = 0; i < image.length; i++) {
     for (var j = 0; j < image[i].length; j++) {
       var rgbString = image[i][j]
       var rgbNumbers = rgbStringToArray(rgbString)
@@ -47,7 +47,23 @@ function applyFilter(filterFunction) {
 }
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[0][0]
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      var rgbString = image[i][j]
+      if (rgbString === backgroundColor) {
+        return
+      }
+      else {
+        var rgbNumbers = rgbStringToArray(rgbString)
+        filterFunction(rgbNumbers)
+        rgbString = rgbArrayToString(rgbNumbers)
+        image[i][j] = rgbString
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
 function keepInBounds(num) {
